056d8b8b9ddc557ca82ad409489dc7db30186ca3
Oliver Lee <mainfolder@Olivers-MacBook-Pro-2.local>
2020-09-09 13:46:46 -0700

    First version  of word_games.py

---
+++ word_games.py
@@ -0,0 +1,14 @@
+"""
+Options
+1) write a program that computes a scrabble score of a word
+
+"""
+letter_values = {' ': 0, 'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g':  2, 'h': 4, 'i': 1, 'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1, 's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10}
+word = input("Input a word -- no punctuation\n")
+word = word.lower()
+score = 0
+for i in range(0, len(word)):
+	letter = word[i]
+	score += letter_values[letter]
+print(score)
+	
